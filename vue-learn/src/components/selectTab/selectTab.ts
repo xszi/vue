0914527e3ecb1db7f8481af22f7cea21b717f4f4
xszi <jiuhuaw@126.com>
2021-01-05 11:20:10 +0800

    selectTab

--- vue-learn/src/components/selectTab/selectTab.ts
+++ vue-learn/src/components/selectTab/selectTab.ts
@@ -13,7 +13,20 @@ export default class SelectTab extends Vue {
 
     tab = 'first';
 
-    changeActiveTab () {
-        console.log(123456);
+    changeActiveTab (tabName: string) {
+        this.tab = tabName;
+        const bar = <HTMLElement>document.getElementsByClassName('tabs-active-bar')[0];
+        // ts语法需要加类型断言<HTMLElement>
+        const tab = <HTMLElement>this.$refs[`tab-${tabName}`];
+        console.log(tab, 'dddd');
+        if (!bar || !tab || !tab.querySelector('.text')) {
+            return;
+        }
+        const left = tab.offsetLeft;
+        // 使用非空断言符号tab.querySelector('.text')!
+        const { width } = tab.querySelector('.text')!.getBoundingClientRect();
+        const barWidth = bar.getBoundingClientRect().width;
+        const x = left + (width - barWidth) / 2;
+        bar.style.transform = `translate(${x}px)`
     }
 }
